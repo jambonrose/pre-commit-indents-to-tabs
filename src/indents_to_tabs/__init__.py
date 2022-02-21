097675939ba8f8fa517a2396dd8840ee25f6a83b
Andrew Pinkham <code@andrewsforge.com>
2022-02-21 14:55:08 -0500

    ✨ Add command-line handling logic in project

--- src/indents_to_tabs/__init__.py
+++ src/indents_to_tabs/__init__.py
@@ -1,3 +1,34 @@
 """Replace spaces with tabs in lines that begin with spaces."""
+from __future__ import annotations
+
+import argparse
+from typing import Sequence
+
+from .convert import convert_indents
 
 __version__ = "0.0.1"
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    """Convert indents when invoked from command line"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "filenames",
+        nargs="+",
+        type=argparse.FileType("rb+"),
+        help="Files to convert",
+    )
+    parser.add_argument(
+        "--spaces",
+        type=int,
+        default=2,
+        metavar="INTEGER",
+        help="How many spaces to replace with a tab",
+    )
+    args = parser.parse_args(argv)
+
+    return convert_indents(args.filenames, args.spaces)
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
